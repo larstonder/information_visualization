54c4f2f3e0b330d4fafb929909ef0daf2f14ddd1
Lars Tønder <lars.tonder@outlook.com>
2023-02-14 11:22:22 +0100

    Add all pages (some still empty)

--- pages/2_The_project.py
+++ pages/2_The_project.py
@@ -32,31 +32,35 @@ def decrement_page():
 def increment_page():
     st.session_state.page += 1
 
-#-------Page start-------
+def restart():
+    st.session_state.page = 0
 
-if st.session_state.page == 0:
-    text_block = st.markdown("""
-    # Murder history
-    The project “Murder History” is dedicated to serial killers, their histories, motives,
-    and analysis related to origin, sex, occupation of murders.
-    Referring to the datasets made publicly available by the wikidata,
-    we are aiming to understand which criteria have been decisive in committing murders.
+# --- Page start ---
 
-    **The investigation tries to answer some question to better cover the topic:**
+if st.session_state.page == 0: # INTRO
+    st.markdown("""
+        # Murder history
+        The project “Murder History” is dedicated to serial killers, their histories, motives,
+        and analysis related to origin, sex, occupation of murders.
+        Referring to the datasets made publicly available by the wikidata,
+        we are aiming to understand which criteria have been decisive in committing murders.
 
-    * Which countries do the most serial killers originate from? 
-    * How many percent is the number of serial killers per 100 thousand of the population of these countries? 
-    * How many of them are men and women?
-    * How many of them had higher education? 
-    * What was their occupation? 
-    * How many of them had mental conditions? 
-    * Who had the biggest number of victims? 
-    * What penalties did they receive? 
+        **The investigation tries to answer some question to better cover the topic:**
+
+        * Which countries do the most serial killers originate from? 
+        * How many percent is the number of serial killers per 100 thousand of the population of these countries? 
+        * How many of them are men and women?
+        * How many of them had higher education? 
+        * What was their occupation? 
+        * How many of them had mental conditions? 
+        * Who had the biggest number of victims? 
+        * What penalties did they receive? 
     """)
-elif st.session_state.page == 1:
-    text_block = st.markdown("""
-    # Origins
-    As shown in the map below, ... 
+
+elif st.session_state.page == 1: # ORIGINS
+    st.markdown("""
+        # Origins
+        As shown in the map below, ... 
     """)
     fig = px.choropleth(
         countries,
@@ -72,30 +76,57 @@ elif st.session_state.page == 1:
     bar = px.bar(countries, y='Total', x='Country')
     st.plotly_chart(bar, use_container_width=True)
 
-# pie = px.pie(demographics, values='Number of people', names='Demographic')
-# bar = px.bar(demographics.sort_values(by=['Number of people']), y='Number of people', x='Demographic')
-# fig = px.choropleth(
-#     countries,
-#     locations="Country Code",
-#     color="Total",
-#     hover_name="Country",
-#     color_continuous_scale=px.colors.sequential.Plasma)
+elif st.session_state.page == 2:
+    st.markdown("""
+        # Serial killers per capita
+    """)
 
-# if st.checkbox('Show temporary charts'):
-#     st.plotly_chart(pie, use_container_width=True)
-#     st.plotly_chart(bar, use_container_width=True)
-#     st.plotly_chart(fig, use_container_width=True)
+elif st.session_state.page == 3:
+    st.markdown("""
+        # Divison between genders
+    """)
 
-# if st.checkbox('Show dataframes'):
-#     for i, (key, df) in enumerate(dfs.items()):
-#         f"## {key}"
-#         st.write(df)
+elif st.session_state.page == 4:
+    st.markdown("""
+        # Education
+    """)
+
+elif st.session_state.page == 5:
+    st.markdown("""
+        # Occupations
+    """)
+
+elif st.session_state.page == 6:
+    st.markdown("""
+        # Mental conditions
+    """)
+
+elif st.session_state.page == 7:
+    st.markdown("""
+        # Victims
+    """)
+
+elif st.session_state.page == 8:
+    st.markdown("""
+        # Penalties
+    """)
+
+else:
+    st.markdown("""
+        # Thank you for joining us on this journey!
+    """)
+
+# --- Buttons ---
 
 but1, but2, _ = st.columns((2,2,8))
 
-if st.session_state.page !=0:
-    with but1: st.button('Previous', on_click=decrement_page)
-    with but2: st.button('Next', on_click=increment_page)
-else:
+if st.session_state.page == 0:
     but1, but2, _ = st.columns((10,1,1))
     with but1: st.button('Start journey', on_click=increment_page)
+elif st.session_state.page > 8:
+    but1, but2, _ = st.columns((2,3,7))
+    with but1: st.button('Previous', on_click=decrement_page)
+    with but2: st.button('Restart journey', on_click=restart)
+else:
+    with but1: st.button('Previous', on_click=decrement_page)
+    with but2: st.button('Next', on_click=increment_page)
